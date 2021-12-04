7f2ff7206f2cff76de486ab3bf80137f95a9b7e2
sam mix <1045262335@qq.com>
2021-12-05 02:37:11 +0800

    tidy

---
+++ cart/index.js
@@ -0,0 +1,31 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        list: [
+            {
+                id : 1,
+                name: 'iPhone 7',
+                price: 6188,
+                count: 1,
+            },
+            {
+                id : 2,
+                name: 'iPad Pro',
+                price: 5888,
+                count: 1,
+            },
+            {
+                id : 3,
+                name: 'MacBook Pro',
+                price: 21488,
+                count: 1,
+            },
+        ]
+    },
+    computed: {
+
+    },
+    methods: {
+
+    }
+})
